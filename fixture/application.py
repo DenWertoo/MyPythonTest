da3e8d5f38cd5922f6b78de358c84bb8f21d480a
DenWertoo <aldoraygreat@yandex.ru>
2016-03-09 18:57:20 +0300

    5.1 lection not finished

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -11,6 +12,7 @@ class Application:
         # self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def is_valid(self):
         try:
